c705b5d12d2f35f9d400d1c09fdf58215a0e99d7
RED <rasg3000@hotmail.com>
2018-07-07 15:31:04 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+//MODULES
+import { PagesModule } from './components/pages/pages.module';
 
-
-import { LoginComponent } from './components/pages/login/login.component';
-import { NopagefoundComponent } from './components/shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-import { ProgressComponent } from './components/pages/progress/progress.component';
-import { Graficas1Component } from './components/pages/graficas1/graficas1.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
-import { BreadcrubsComponent } from './components/shared/breadcrubs/breadcrubs.component';
+//ROUTES
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './components/pages/pages.component';
+
+//COMPONENTS
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
 import { RegisterComponent } from './components/pages/login/register.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrubsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    PagesModule,
+    APP_ROUTES,
+    
 
   ],
   providers: [],
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: []
+  styles: [],
+
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
+    
   }
 
 }
